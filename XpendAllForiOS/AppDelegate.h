2dbe1d42e76749830a6f74a8fffc2543743fae35
bird Chiu <bird1204@gmail.com>
2013-09-28 00:45:57 +0800

    Initial Commit

---
+++ XpendAllForiOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XpendAllForiOS
+//
+//  Created by BirdChiu on 13/9/28.
+//  Copyright (c) 2013年 BirdChiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XpendAllForiOSTests/XpendAllForiOSTests.m
@@ -0,0 +1,34 @@
+//
+//  XpendAllForiOSTests.m
+//  XpendAllForiOSTests
+//
+//  Created by BirdChiu on 13/9/28.
+//  Copyright (c) 2013年 BirdChiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XpendAllForiOSTests : XCTestCase
+
+@end
+
+@implementation XpendAllForiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
